e56e112095d761b58fc8323bb18acc92b0904bff
snorflib <modicum.geotag.0v@icloud.com>
2024-09-25 03:48:00 +0200

    fix(tests): 3.10 + pytest

--- src/paramio/_field.py
+++ src/paramio/_field.py
@@ -14,15 +14,6 @@ if typing.TYPE_CHECKING:
 OutType = typing.TypeVar("OutType")
 
 
-class FieldParams(typing.Generic[OutType], typing.TypedDict, total=False):
-    default: OutType
-    prefix: str
-    key: str
-    reader: types.ReaderType[str, typing.Any] | None
-    conv: types.ConverterType[typing.Any, typing.Any] | None
-    secret: bool
-
-
 @dataclasses.dataclass(slots=True)
 class Field:
     default: typing.Any = _internal.SENTINEL
@@ -35,5 +26,13 @@ class Field:
     type_: typing.Any = dataclasses.field(init=False)
 
 
-def field(**kwargs: t_ext.Unpack[FieldParams[OutType]]) -> OutType:
+class FieldParams(typing.TypedDict, total=False):
+    prefix: str
+    key: str
+    reader: types.ReaderType[str, typing.Any] | None
+    conv: types.ConverterType[typing.Any, typing.Any] | None
+    secret: bool
+
+
+def field(default: OutType, **kwargs: t_ext.Unpack[FieldParams]) -> OutType:
     return Field(**kwargs)  # type: ignore
