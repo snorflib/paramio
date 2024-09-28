96da8596e06eb5e4603d52e1dafff5fc11a1aa43
snorflib <modicum.geotag.0v@icloud.com>
2024-09-28 18:57:05 +0200

    chore: view -> package

---
+++ src/paramio/views/read.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+# Ruff complaints when overload is not from the standard library
+from typing import overload
+
+from src.paramio import prototype, types
+from src.paramio._internal import typing
+from src.paramio.types import var
+
+Self = typing.TypeVar("Self")
+Inst = typing.TypeVar("Inst", bound=prototype.Prototype)
+
+
+class ReadOnlyView(types.ViewType[Inst, var.InType, var.OutType]):
+    __slots__ = ("_name",)
+
+    @overload
+    def __get__(self, instance: None, owner: type[Inst]) -> typing.Self: ...
+    @overload
+    def __get__(self, instance: Inst, owner: type[Inst]) -> var.OutType: ...
+    def __get__(self, instance: Inst | None, owner: type[Inst]) -> var.OutType | typing.Self:
+        if instance is None:
+            return self
+
+        return typing.cast(var.OutType, instance.__internal__[self._name])
+
+    def __set__(self, instance: Inst, value: var.InType) -> typing.NoReturn:
+        raise NotImplementedError(f"{type(self).__name__!r} is read only.")
+
+    def __set_name__(self, owner: type[Inst], name: str) -> None:
+        self._name = name
